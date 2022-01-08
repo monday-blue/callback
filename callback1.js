5bc30141191b29d7ffee3dad5e8fab2eb07a2c40
monday-blue <tflying77@gmail.com>
2022-01-09 07:38:56 +0800

    review push

--- callback1.js
+++ callback1.js
@@ -8,24 +8,49 @@ let doWork = function (job, timer, callback) {
   };
   
   // 刷牙 --> 吃早餐 --> 寫功課
+  // let dt = new Date();
+  // console.log(`Start ${dt.toISOString()}`);
+  // doWork("刷牙", 2000, (err, result) => {
+  //   // 刷牙 --> 吃早餐 --> 寫功課
+  //   let dt = new Date();
+  //   console.log(`${result} at ${dt.toISOString()}`);
+  // });
+  
+  // doWork("吃早餐", 3000, (err, result) => {
+  //   let dt = new Date();
+  //   console.log(`${result} at ${dt.toISOString()}`);
+  // });
+  
+  // doWork("寫功課", 2000, (err, result) => {
+  //   let dt = new Date();
+  //   console.log(`${result} at ${dt.toISOString()}`);
+  // });
+
+
+let dt = new Date();
+console.log(`Start ${dt.toISOString()}`);
+
+doWork("刷牙", 2000, (err, result) => {
+
+  // 刷牙 --> 吃早餐 --> 寫功課
   let dt = new Date();
-  console.log(`Start ${dt.toISOString()}`);
-  doWork("刷牙", 2000, (err, result) => {
-    // 刷牙 --> 吃早餐 --> 寫功課
-    let dt = new Date();
-    console.log(`${result} at ${dt.toISOString()}`);
-  });
+  console.log(`${result} at ${dt.toISOString()}`);
   
   doWork("吃早餐", 3000, (err, result) => {
     let dt = new Date();
     console.log(`${result} at ${dt.toISOString()}`);
-  });
-  
-  doWork("寫功課", 2000, (err, result) => {
-    let dt = new Date();
-    console.log(`${result} at ${dt.toISOString()}`);
+
+    doWork("寫功課", 2000, (err, result) => {
+      let dt = new Date();
+      console.log(`${result} at ${dt.toISOString()}`);
+    });
+
   });
 
+});
+
+
+// 想法不對
 //   function delay() {
 //       let now = new Date().getTime();  // 抓現在的秒數
 //       console.log(now);
